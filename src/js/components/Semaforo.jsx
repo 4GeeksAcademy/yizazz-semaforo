266bf7751228b6b4332b9d60a8ce029f3782aa73
yizazz <jesusastorga53@gmail.com>
2025-09-10 12:26:46 +0000

    estructura fisica lista

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -2,13 +2,15 @@ import react, { useState } from "react"
 
 const Semaforo = ()=>{
     return <div id="Box" className="container">
+    <div id="container1">
         <div id="superior" className="row">        
               <div id="left" className="col-5"></div>
-              <span className="col-2"></span>
+              <span id="middle" className="col-2"></span>
               <div id="right"className="col-5"></div>
       
         </div>
-        <div id="bordeSuperior" className="row">
+    </div>
+        <div id="container2" className="row">
           <div id="Lights">
             <div id="red"></div>
             <div id="orange"></div>
